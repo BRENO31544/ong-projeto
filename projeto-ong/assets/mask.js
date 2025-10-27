ef6b37d5238bf225062b1da1ce48345dbf0648b4
Breno FElix <breno.31544@gmail.com>
2025-10-26 21:58:57 -0300

    Desenvolvimento ong

---
+++ projeto-ong/assets/mask.js
@@ -0,0 +1,23 @@
+// Máscara CPF
+document.getElementById("cpf").addEventListener("input", function() {
+    let value = this.value.replace(/\D/g, '');
+    if (value.length > 3) value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    if (value.length > 6) value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    if (value.length > 9) value = value.replace(/(\d{3})(\d)/, "$1-$2");
+    this.value = value;
+});
+
+// Máscara telefone
+document.getElementById("telefone").addEventListener("input", function() {
+    let value = this.value.replace(/\D/g, '');
+    value = value.replace(/(\d{2})(\d)/, "($1) $2");
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
+    this.value = value;
+});
+
+// Máscara CEP
+document.getElementById("cep").addEventListener("input", function() {
+    let value = this.value.replace(/\D/g, '');
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
+    this.value = value;
+});
